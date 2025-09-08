31c9e4f26a96e865787aac6c13204205df782f9e
GustavoK1510 <gustavoklc1510@gmail.com>
2025-09-08 09:11:04 -0300

    Layout IMC

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -11,30 +11,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    EditText edmin, edmax;
-    TextView tvResultado;
+    Button button;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        edmin=findViewById(R.id.edmin);
-        edmax=findViewById(R.id.edmax);
-        tvResultado=findViewById(R.id.tvResultado);
-        Button b=findViewById(R.id.button);
-
-        b.setOnClickListener(v -> {
-            int min = Integer.parseInt(edmin.getText().toString());
-            int max = Integer.parseInt(edmax.getText().toString());
-            //Random
-            Random random=new Random();
-
-            int n=random.nextInt(max-min)+min;
-
-            tvResultado.setText(Integer.toString(n));
-
-        });
-
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);this);
     }
+
 
 
 }
