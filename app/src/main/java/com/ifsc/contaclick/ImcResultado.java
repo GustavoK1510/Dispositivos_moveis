7fcfaaac9fb340ff9fad7e32c107f18d2d23c029
GustavoK1510 <gustavoklc1510@gmail.com>
2025-09-15 09:08:11 -0300

    imc

--- app/src/main/java/com/ifsc/contaclick/ImcResultado.java
+++ app/src/main/java/com/ifsc/contaclick/ImcResultado.java
@@ -1,6 +1,10 @@
 package com.ifsc.contaclick;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ImcResultado extends AppCompatActivity {
+    Button b;
+
+    TextView txtIMC;
+
+    ImageView imageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        txtIMC = findViewById(R.id.txtIMC);
+        imageView2 = findViewById(R.id.imageView2);
         setContentView(R.layout.activity_imc_resultado);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        double peso = b.getDouble("peso");
+        double metros = b.getDouble("metros");
+
+
+        double imc = peso/(metros*metros);
+
+        txtIMC.setText(Double.toString(imc));
     }
 }
--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,21 +1,35 @@
 package com.ifsc.contaclick;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
 
+    EditText edPeso, edMetros;
     @Override
     protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);this);
+        edPeso = findViewById(R.id.edPeso);
+        edMetros = findViewById(R.id.edMetros);
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(v->{
+            Intent i = new Intent(this, ImcResultado.class);
+            Bundle b = new Bundle();
+            double peso = Double.parseDouble(edPeso.getText().toString());
+            double metros = Double.parseDouble(edMetros.getText().toString());
+            b.putDouble("peso",peso);
+            b.putDouble("metros",metros);
+            i.putExtras(b);
+            startActivity(i);
+        });
     }
 
 
