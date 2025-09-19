e8189324ce6e8716e4e44e26e9e9bdc182a22eca
GustavoK1510 <gustavoklc1510@gmail.com>
2025-09-19 09:58:51 -0300

    Hello Internacionalizado

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,7 +1,6 @@
 package com.ifsc.contaclick;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,28 +10,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    EditText edmin, edmax;
-    TextView tvResultado;
+    TextView tvHello;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edmin=findViewById(R.id.edmin);
-        edmax=findViewById(R.id.edmax);
-        tvResultado=findViewById(R.id.tvResultado);
-        Button b=findViewById(R.id.button);
-
-        b.setOnClickListener(v -> {
-            int min = Integer.parseInt(edmin.getText().toString());
-            int max = Integer.parseInt(edmax.getText().toString());
-            //Random
-            Random random=new Random();
-
-            int n=random.nextInt(max-min)+min;
-
-            tvResultado.setText(Integer.toString(n));
-
-        });
+        tvHello=findViewById(R.id.tvHello);
 
     }
 
